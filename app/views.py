b88e8a1177cc2fa52086d946954b9e30c42d1aa8
Kamalpandi <kamalpandi123@gmail.com>
2021-12-24 19:12:53 +0530

    emtemp vibi

--- app/views.py
+++ app/views.py
@@ -3,12 +3,18 @@ from django.db.models.query import QuerySet
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAuthenticated ,IsAdminUser
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 from rest_framework import pagination, serializers, viewsets, generics
-from .models import Booking, BusinessPartner, Payment, User,Slots,Wing
-from .serializers import GetUserSerializer,PaymentSerializer,BusinessGroup_Serializer, UserSerializer,wingSlotSerializer,SlotSerializer, BusinessSerializer, BookingSerializer,AdminCheckSerializer
+from .models import Booking, BusinessPartner, Payment, User, Slots, Wing
+from .serializers import GetUserSerializer, PaymentSerializer, BusinessGroup_Serializer, UserSerializer, wingSlotSerializer, SlotSerializer, BusinessSerializer, BookingSerializer, AdminCheckSerializer
 from rest_framework import filters
-from .pagination import SmallSetPagination,tenSetPagination,fourSetPagination   
+from .pagination import SmallSetPagination, tenSetPagination, fourSetPagination
+
+from django.core.mail import send_mail
+from django.conf import settings
+from django.template import loader
+from django.template.loader import render_to_string
+
 
 @api_view()
 @permission_classes([AllowAny])
@@ -23,8 +29,52 @@ def SlotCount(request):
     total = Slots.objects.count()
     active = Slots.objects.filter(slotStatus=True).count()
     inactive = Slots.objects.filter(slotStatus=False).count()
-    return Response({'total':total,'inactive':inactive,'active':active})
+    return Response({'total': total, 'inactive': inactive, 'active': active})
 
+
+@api_view(['GET', 'POST'])
+@permission_classes([AllowAny])
+def send_gmail(request):
+    data = {
+        # "to":"kamalpandi123@gmail.com",
+        "invoiceDate": "payment_invoice.paymentData.paymentDate",
+        "user": "payment_invoice.User.userName",
+        "accountNumber": "payment_invoice.User.accountNumber",
+        "paymentId": "payment_invoice.paymentId",
+        "amount": "payment_invoice.amount"
+    }
+    messageSent = False
+    if request.method == "POST":
+
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        from_mail = settings.EMAIL_HOST_USER  # settings.py
+        to_mail = request.POST.get('to')  # to must be posted in postman
+
+        msg_html = loader.render_to_string('index.html', {
+
+            'to': to_mail,
+            'invoiceDate': data["invoiceDate"],
+            'user': data["user"],
+            'accountNumber': data["accountNumber"],
+            'paymentId': data["paymentId"],
+            'amount': data["amount"],
+        })
+
+        send_mail(
+            subject,
+            message,
+            from_mail,
+            [to_mail],
+            fail_silently=False,
+            html_message=msg_html,
+        )
+        messageSent = True
+
+        return Response({"sent": messageSent})
+
+    else:
+        return Response({"sent": messageSent})
 
 
 class UserCreateAPIView(generics.CreateAPIView):
@@ -32,62 +82,63 @@ class UserCreateAPIView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
 
+
 class GetUserAccount(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = GetUserSerializer
-    permission_classes=[IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         queryset = self.queryset
         query_set = queryset.filter(id=self.request.user.id)
         return query_set
 
+
 class AdminCheckAPI(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = AdminCheckSerializer
-    permission_classes=[IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         queryset = self.queryset
-        query_set = queryset.all()#filter(id=self.request.user.id)
+        query_set = queryset.all()  # filter(id=self.request.user.id)
         return query_set
-
-
 
 
 class CreatePayment(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
 
+
 class CreateBusinessPartner(viewsets.ModelViewSet):
-     permission_classes=[IsAuthenticated]
-     queryset = BusinessPartner.objects.all()
-     serializer_class = BusinessSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = BusinessPartner.objects.all()
+    serializer_class = BusinessSerializer
 
 
 class GetBusinessPartner(viewsets.ModelViewSet):
-     permission_classes=[IsAuthenticated]
-     queryset = BusinessPartner.objects.all()
-     serializer_class = BusinessGroup_Serializer
-     pagination_class = tenSetPagination
+    permission_classes = [IsAuthenticated]
+    queryset = BusinessPartner.objects.all()
+    serializer_class = BusinessGroup_Serializer
+    pagination_class = tenSetPagination
+
 
 class Check_BusinessPartner(generics.ListAPIView):
-     queryset = BusinessPartner.objects.all()
-     serializer_class = BusinessSerializer
-     permission_classes=[IsAuthenticated]
-     filter_backends = [filters.SearchFilter,filters.OrderingFilter]
-     search_fields = ['userName']
-     ordering = ('-BusinessPartner_created')
-     pagination=tenSetPagination
+    queryset = BusinessPartner.objects.all()
+    serializer_class = BusinessSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['userName']
+    ordering = ('-BusinessPartner_created')
+    pagination = tenSetPagination
 
 
 class BusinessPartner_Group(viewsets.ModelViewSet):
-     queryset = BusinessPartner.objects.all()
-     serializer_class = BusinessGroup_Serializer
-     permission_classes=[IsAuthenticated]
-     filter_backends = [filters.SearchFilter]
-     search_fields = ['userName']
-
+    queryset = BusinessPartner.objects.all()
+    serializer_class = BusinessGroup_Serializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['userName']
 
 
 class UserLogin(generics.ListAPIView):
@@ -99,7 +150,8 @@ class UserLogin(generics.ListAPIView):
         queryset = self.queryset
         username = self.request.query_params.get('username')
         accountnumber = self.request.query_params.get('accountnumber')
-        query_set = queryset.filter(userName=username,accountNumber=accountnumber)
+        query_set = queryset.filter(
+            userName=username, accountNumber=accountnumber)
         return query_set
 
 # ------------------------------------------------------------------------------------------------------------------
@@ -108,28 +160,28 @@ class UserLogin(generics.ListAPIView):
 class CreatePayment(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
-    pagination_class=tenSetPagination
+    pagination_class = tenSetPagination
     lookup_field = 'id'
     filter_backends = [filters.OrderingFilter]
     ordering = ('-paymentDate')
 
 
-
 class GetPayment(generics.ListAPIView):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
-    permission_classes=[IsAuthenticated]
-    filter_backends = [filters.SearchFilter,filters.OrderingFilter]
-    search_fields = ['paymentId','userId__userName',]
+    permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['paymentId', 'userId__userName', ]
     ordering = ('-paymentDate')
-    pagination=tenSetPagination
-    
+    pagination = tenSetPagination
+
 
 class CreateBooking(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     filter_backends = [filters.OrderingFilter]
     ordering = ('-date_auto')
+
 
 class GetBooking(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
@@ -140,7 +192,8 @@ class GetBooking(viewsets.ModelViewSet):
         from_date = self.request.query_params.get('from')
         to_date = self.request.query_params.get('to')
         print(from_date)
-        query_set = queryset.filter(endTo__range=[from_date,to_date]).order_by('-date_auto')
+        query_set = queryset.filter(
+            endTo__range=[from_date, to_date]).order_by('-date_auto')
         return query_set
 
 
@@ -149,15 +202,15 @@ class GetBookingByDate(viewsets.ModelViewSet):
     serializer_class = BookingSerializer
     filter_backends = [filters.OrderingFilter]
     ordering = ('-date_auto')
+
     def get_queryset(self):
         queryset = self.queryset
         from_date = self.request.query_params.get('from')
         to_date = self.request.query_params.get('to')
         print(from_date)
-        query_set = queryset.filter(date__range=[from_date,to_date]).order_by('-date_auto')
+        query_set = queryset.filter(
+            date__range=[from_date, to_date]).order_by('-date_auto')
         return query_set
-
-
 
 
 class CreateWing(viewsets.ModelViewSet):
@@ -169,10 +222,11 @@ class CreateSlots(viewsets.ModelViewSet):
     queryset = Slots.objects.all()
     serializer_class = SlotSerializer
 
+
 class GetWing(generics.ListAPIView):
     queryset = Wing.objects.all()
     serializer_class = wingSlotSerializer
-    permission_classes=[IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     filter_backends = [filters.SearchFilter]
     search_fields = ['wingName']
 
@@ -180,14 +234,12 @@ class GetWing(generics.ListAPIView):
 class Inactiveslots(viewsets.ModelViewSet):
     queryset = Slots.objects.all()
     serializer_class = SlotSerializer
-    filter_backends = [filters.SearchFilter,filters.OrderingFilter]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
 
     def get_queryset(self):
         queryset = self.queryset
         query_set = queryset.filter(slotStatus=False)
         return query_set
-
-
 
 
 class Booked_slots(generics.ListAPIView):
@@ -198,4 +250,4 @@ class Booked_slots(generics.ListAPIView):
         queryset = self.queryset
         id = self.request.query_params.get('id')
         query_set = queryset.filter(slotId=id)
-        return query_set    
+        return query_set
