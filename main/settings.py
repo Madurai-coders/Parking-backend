7ec13966440c38985cdd953fae3dc88797d9938a
Madurai-coders <81981841+Madurai-coders@users.noreply.github.com>
2022-10-11 16:44:04 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bc%3^l#dhye^$5=!d=sx@7b1i(i19k8jh#0!rl=m9)sddzv$e^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
